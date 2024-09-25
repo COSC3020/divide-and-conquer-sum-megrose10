35652446e97ad08b49e1077de61ea1e9bb420bff
megrose10 <157438545+megrose10@users.noreply.github.com>
2024-09-25 15:40:45 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,39 @@
 function divideAndConquerSum(a) {
+    let elementNum = a.length;
+    if((elementNum == 0) || (elementNum == 1)){
+        return a;
+    } 
+    else{
+        a = mergesort();
+    }
     return a;
+}
+
+function msort(x, lo, hi, tmp) {
+    if(lo >= hi) return;
+    var mid = Math.floor((lo+hi)/2);
+    msort(x, lo, mid, tmp);
+    msort(x, mid+1, hi, tmp);
+    msort(x, mid+2, hi, tmp);
+    merge(x, lo, mid, hi, tmp);
+    }
+
+function mergesort(x) {
+    var tmp = [];
+    msort(x, 0, x.length - 1, tmp);
+}
+    
+function merge(x, lo, mid, hi, tmp) {
+    var a = lo, b = mid + 1;
+    for(var k = lo; k <= hi; k++) {
+    if(a <= mid && (b > hi || x[a] < x[b])) {
+    tmp[k] = x[a++];
+    } 
+    else {
+    tmp[k] = x[b++];
+    }
+    }
+    for(var k = lo; k <= hi; k++) {
+    x[k] = tmp[k];
+    }
 }
