40657696c13a4069996b0aebe79dda8365a267ab
megrose10 <157438545+megrose10@users.noreply.github.com>
2024-10-31 12:01:17 -0600

    Update code.js

--- code.js
+++ code.js
@@ -4,44 +4,48 @@ function divideAndConquerSum(a) {
         return a;
     }
     else {
-        a = mergesort(a);
+        a = mergearrays(a);
     }
     
     return a;
 }
 
-function msort(x, lo, hi, tmp) {
+function mseparate(x, lo, hi, tmp) {
+    //console.log(`${lo}`);
     let athird = Math.floor((lo+hi)*(1/3));
+    //console.log(`${athird}`);
     let atwothird = Math.floor((lo+hi) * (2/3));
-    msort(x, lo, athird, tmp);
-    msort(x, athird + 1, atwothird + 1, tmp);
-    msort(x, atwothird + 1, hi, tmp);
+    //console.log(`${atwothird}`);
+    merge(x, lo, athird, atwothird, hi, tmp)
 }
 
-function mergesort(a) {
+function mergearrays(a) {
     let tmpArray = [];
-
-    msort(a, 0, a.length - 1, tmpArray);
+    //console.log(`${a.length}`);
+    mseparate(a, 0, a.length, tmpArray);
 }
 
 
 function merge(a, low, third1, third2, hi, tmp) {
     let x = low, y = third1 + 1, z = third2 + 1;
     let sum = 0;
-    for(let i = low; i <= third1; i++) {
+    for(let i = low; i < third1; i++) {
+        sum += a[i];
+        //console.log("for" + `${sum}`);
+        //console.log("variable: " + `${third1}`);
+    }
+    //console.log(`${sum}`);
+    for(let i = third1; i < third2; i++) {
+        sum += a[i];
+        //console.log("for" + `${sum}`);
+        //console.log("variable: " + `${third1}`);
+    }
+    //console.log(`${sum}`);
+    for(let i = third2; i < hi; i++) {
         sum += a[i];
     }
-    for(let i = third1; i <= third2; i++) {
-        sum += a[i];
-    }
-    for(let i = third2; i <= hi; i++) {
-        sum += a[i];
-    }
+    //console.log(`${sum}`);
 }
-let array = [1,2,3,4,5];
-divideAndConquerSum(array);
-    for(let i = third2; i <= hi; i++) {
-        sum += a[i];
-    }
-}
+//let array = [1,2,3,4,5];
+//divideAndConquerSum(array);
 
