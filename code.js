10cda910bf3fd414a14114958de8752423d912c8
megrose10 <157438545+megrose10@users.noreply.github.com>
2024-11-06 16:44:22 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,11 @@
 function divideAndConquerSum(a) {
     let alength = a.length;
     let sum = 0;
+    let athird = Math.floor((a.length)*(1/3));
+    let atwothird = Math.floor((a.length) * (2/3));
+    let aThirdArray = [];
+    let aTwoThirdArray = [];
+    let aLastThirdArray = [];
     if(alength == 0) {
         sum = 0;
     }
@@ -11,46 +16,20 @@ function divideAndConquerSum(a) {
         sum = a[0] + a[1];
     }
     else {
-        sum = mergearrays(a);
+        for(let i = 0; i < athird; i++) {
+          aThirdArray.push(a[i]);
+        }
+        for(let i = athird; i < atwothird; i++) {
+          aTwoThirdArray.push(a[i]);
+        }
+        for(let i = atwothird; i < a.length; i++) {
+          aLastThirdArray.push(a[i]);
+        }
+        let sum1 = divideAndConquerSum(aThirdArray);
+        let sum2 = divideAndConquerSum(aTwoThirdArray);
+        let sum3 = divideAndConquerSum(aLastThirdArray);
+        sum = sum1 + sum2 + sum3;
     }
     
     return sum;
 }
-
-function mseparate(x, lo, hi) {
-    //console.log(`${lo}`);
-    let athird = Math.floor((lo+hi)*(1/3));
-    //console.log(`${athird}`);
-    let atwothird = Math.floor((lo+hi) * (2/3));
-    //console.log(`${atwothird}`);
-    return merge(x, lo, athird, atwothird, hi)
-}
-
-function mergearrays(a) {
-    //console.log(`${a.length}`);
-    return mseparate(a, 0, a.length);
-}
-
-
-function merge(a, low, third1, third2, hi) {
-    let sum = 0;
-    for(let i = low; i < third1; i++) {
-        sum += a[i];
-        //console.log("for" + `${sum}`);
-        //console.log("variable: " + `${third1}`);
-    }
-    //console.log(`${sum}`);
-    for(let i = third1; i < third2; i++) {
-        sum += a[i];
-        //console.log("for" + `${sum}`);
-        //console.log("variable: " + `${third1}`);
-    }
-    //console.log(`${sum}`);
-    for(let i = third2; i < hi; i++) {
-        sum += a[i];
-    }
-    //console.log(`${sum}`);
-    return sum;
-}
-//let array = [1,2,3,4,5];
-//console.log(`${divideAndConquerSum(array)}`);
